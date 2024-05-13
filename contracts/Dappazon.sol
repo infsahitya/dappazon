3d545eb07a4fcb9bcaf45da58770b5d7fb421bae
infsahitya <sahitya.api@gmail.com>
2024-05-14 01:13:11 +0530

    update

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -2,13 +2,19 @@
 pragma solidity ^0.8.9;
 
 contract Dappazon {
+    address public storeOwner;
     string public contractName;
 
     constructor(string memory initContractName) {
         contractName = initContractName;
+        storeOwner = msg.sender;
     }
 
     function getContractName() public view returns (string memory) {
         return contractName;
+    }
+
+    function getStoreOwner() public view returns (address) {
+        return storeOwner;
     }
 }
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
@@ -8,16 +9,24 @@ const contractName = "Dappazon";
 
 describe("Dappazon", () => {
   let dappazon;
+  let buyer, deployer;
 
   beforeEach(async () => {
+    [deployer, buyer] = await ethers.getSigners();
+
     const Dappazon = await ethers.getContractFactory(contractName);
     dappazon = await Dappazon.deploy(contractName);
   });
 
   describe("Deployment", () => {
-    it("Contract Name", async () => {
+    it(`Contract - ${contractName}`, async () => {
       const fetchedName = await dappazon.getContractName();
       expect(fetchedName).to.equal(contractName);
+    });
+
+    it(`${contractName} Owner`, async () => {
+      const fetchedOwner = await dappazon.getStoreOwner();
+      expect(fetchedOwner).to.be.equal(deployer.address);
     });
   });
 });
