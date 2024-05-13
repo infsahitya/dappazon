3e646469a8a7bb3808712866bdc26e1cbe5cf2e0
infsahitya <sahitya.api@gmail.com>
2024-05-14 01:38:49 +0530

    updaet

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -5,39 +5,73 @@ contract Dappazon {
     address public storeOwner;
     string public contractName;
 
+    struct Item {
+        uint256 id;
+        string name;
+        string image;
+        uint256 cost;
+        uint256 stock;
+        uint256 rating;
+        string category;
+    }
+
+    mapping(uint256 => Item) items;
+    uint256 private totalItems;
+
     constructor(string memory initContractName) {
         contractName = initContractName;
         storeOwner = msg.sender;
+
+        totalItems = 0;
     }
 
+    // TODO: Get deployed contract's name
     function getContractName() public view returns (string memory) {
         return contractName;
     }
 
+    // TODO: Get deployers address
     function getStoreOwner() public view returns (address) {
         return storeOwner;
     }
 
-    // TODO: List Products
-    function listProducts(
+    // TODO: Add Product
+    function addProduct(
         uint256 _id,
         uint256 _cost,
-        uint256 _stock
+        uint256 _stock,
         uint256 _rating,
         string memory _name,
         string memory _image,
-        string memory _category,
+        string memory _category
     ) public {
+        Item memory item = Item(
+            _id,
+            _name,
+            _image,
+            _cost,
+            _stock,
+            _rating,
+            _category
+        );
 
+        items[_id] = item;
     }
 
     // TODO: Buy Products
-    function buyProduct() public {
-    
-    }
+    function buyProduct() public {}
 
     // TODO: Withdraw Funds
-    function withdrawFunds() public {
-    
+    function withdrawFunds() public {}
+
+    // TODO: List Products
+    function listProducts() public view returns (Item[] memory) {
+        Item[] memory productsList = new Item[](totalItems);
+
+        for (uint256 i = 0; i < totalItems; i++) {
+            productsList[i] = items[i];
+        }
+
+        return productsList;
     }
 }
