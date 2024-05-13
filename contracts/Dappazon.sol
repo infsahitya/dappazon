7417a16127925f55a208bfa64b06f10dc3edda63
infsahitya <sahitya.api@gmail.com>
2024-05-14 00:54:31 +0530

    update

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon { }
+contract Dappazon {
+    string public contractName;
+
+    constructor(string memory initContractName) {
+        contractName = initContractName;
+    }
+
+    function getContractName() public view returns (string memory) {
+        return contractName;
+    }
+}
